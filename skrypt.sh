845004058c4bf51e61831b9270169c5fbe718577
Igor Fabiszak <dsw51706@student.dsw.edu.pl>
2024-05-05 22:58:37 +0200

    zmodyfikowanie skryptu w kroku drugim labolatoria

--- skrypt.sh
+++ skrypt.sh
@@ -1,36 +1,53 @@
 #!/bin/bash
 
-show_date() {
+function show_date() {
   echo "Today's date is: $(date)"
 }
-create_logs() {
-  local number_of_files=$1
-  for i in $(seq 1 $number_of_files); do
+
+function create_logs() {
+  local number=$1
+  for i in $(seq 1 $number); do
     echo "Log for file $i, created on $(date)" > "log$i.txt"
   done
-  echo "Created $number_of_files log files."
+  echo "Created $number log files."
 }
-show_help() {
+
+function create_errors() {
+  local number=$1
+  for i in $(seq 1 $number); do
+    echo "Error in file $i" > "error$i.txt"
+  done
+  echo "Created $number error files."
+}
+
+function help_menu() {
   echo "Available options:"
-  echo "--date: Display today's date"
-  echo "--logs [number]: Create a specified number of log files"
-  echo "--help: Display this help"
+  echo "-h, --help       display this help menu"
+  echo "-d, --date       display today's date"
+  echo "-l [number], --logs [number] create a specified number of log files"
+  echo "--init           clone repository and add path to PATH"
+  echo "-e [number], --error [number] create a specified number of error files"
 }
+
 case "$1" in
-  --date)
+  -h|--help)
+    help_menu
+    ;;
+  -d|--date)
     show_date
     ;;
-  --logs)
-    if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
-      create_logs "$2"
-    else
-      echo "Provide the number of files to create as an argument."
-    fi
+  -l|--logs)
+    create_logs "$2"
     ;;
-  --help)
-    show_help
+  --init)
+    git clone https://github.com/terapeutaLPG/lab4narzedzia
+    cd lab4narzedzia || exit
+    export PATH="$PATH:$(pwd)"
+    ;;
+  -e|--error)
+    create_errors "$2"
     ;;
   *)
-    echo "Invalid option. Try '--help' for more information."
+    echo "Invalid option. Try '-h' or '--help' for more information."
     ;;
 esac
